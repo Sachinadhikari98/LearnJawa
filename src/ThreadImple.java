1a9e5bfa608f6e0272dad4b1a9617f5630922590
Sachin <sachinadh150@gmail.com>
2026-02-09 10:26:14 +0545

    new

---
+++ src/ThreadImple.java
@@ -0,0 +1,11 @@
+public class ThreadImple implements Runnable{
+    static void main(String[] args){
+        ThreadImple i1=new ThreadImple();
+        Thread t1=new Thread(i1);
+        t1.start();
+        System.out.println("Running via CPU");
+    }
+    public void run(){
+        System.out.println("Running via thread");
+    }
+}
