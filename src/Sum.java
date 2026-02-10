fc4a0ef219f4601d551d5f34a1c0fbb9b9a42f1c
Sachin <sachinadh150@gmail.com>
2026-02-10 09:35:17 +0545

    new

---
+++ src/Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+    static void main(){
+        Integer i1=10;
+        Integer i2=20;
+        Character c='c';
+        String s="ram";
+        Boolean y=true;
+        Double f1=10.5;
+        Integer i3=i1+i2;
+        System.out.println(i3);
+        System.out.println(c+" "+s+" "+y+" "+f1);
+    }
+}
