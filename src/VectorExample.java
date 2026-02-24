e6574175d6e486cae68a4e9b80a94c27634ae63f
Sachin <sachinadh150@gmail.com>
2026-02-24 10:24:42 +0545

    new commit

---
+++ src/VectorExample.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+public class VectorExample {
+    static void main(String[] args) {
+        Vector<String> v1=new Vector<>();
+        v1.add("Rame");
+        v1.add("Shyam");
+        v1.add("Hari");
+        v1.add("Gita");
+        v1.add("Bishnu");
+        System.out.println(v1);
+    }
+}
