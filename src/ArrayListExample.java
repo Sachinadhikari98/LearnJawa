fa546f547dd47e7461e4d0465d83fb8961785305
Sachin <sachinadh150@gmail.com>
2026-02-24 10:06:40 +0545

    new commit

---
+++ src/ArrayListExample.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    static void main(String[] args){
+        ArrayList arr=new ArrayList();
+        arr.add("Sachin");
+        arr.add(10);
+        arr.add('c');
+        arr.add(10.5);
+        System.out.println(arr);
+    }
+}
