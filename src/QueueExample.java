e5efeabefdd63b84d8712f2e508cda2c47672015
Sachin <sachinadh150@gmail.com>
2026-02-24 10:19:44 +0545

    new commit

---
+++ src/QueueExample.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+    static void main(String[] args) {
+        Queue<Integer> q1=new LinkedList<>();
+        q1.add(10);
+        q1.add(20);
+        q1.add(30);
+        System.out.println(q1);
+        System.out.println(q1.peek());
+        q1.remove();
+        System.out.println(q1);
+
+    }
+}
