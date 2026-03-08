972b8237c221c5b768ff579e4621ee026c1ba85f
Sachin <sachinadh150@gmail.com>
2026-03-08 07:33:42 +0545

    new commit

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class RandomNumber {
     static void main(String[] args) {
         Random r1=new Random();
-        int num= r1.nextInt(10);
+        int num= r1.nextInt(100);
         System.out.println(num);
     }
 }
