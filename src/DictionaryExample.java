996cd58768eb547bed94a864add7920e396bdc82
Sachin <sachinadh150@gmail.com>
2026-03-08 07:48:40 +0545

    new commit

--- src/DictionaryExample.java
+++ src/DictionaryExample.java
@@ -1,6 +1,6 @@
 import java.util.Dictionary;
 import java.util.Hashtable;
-
+enum Days{ab,ad,jd,uh,hd};
 public class DictionaryExample {
     static void main(String[] args) {
         Dictionary<String,String> d1=new Hashtable<>();
