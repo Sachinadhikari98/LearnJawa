53970b55c91cef23e2e8e6b8c08f46610d11d0b4
Sachin <sachinadh150@gmail.com>
2026-03-08 08:08:44 +0545

    new commit

--- src/ComparatorIterator.java
+++ src/ComparatorIterator.java
@@ -1,6 +1,5 @@
 import javax.swing.text.html.HTMLDocument;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.*;
 
 public class ComparatorIterator {
     static void main(String[] args) {
@@ -14,5 +13,12 @@ public class ComparatorIterator {
         while(it1.hasNext()){
             System.out.println(it1.next());
         }
+        Collections.sort(i1, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a1, Integer a2) {
+                return a1-a2;
+            }
+        });
+        System.out.println(i1);
     }
 }
